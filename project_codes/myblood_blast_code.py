922aca9f822b3b32a6bbcac063983dec7de9a7b2
syahiramzar <128501870+syahiramzar@users.noreply.github.com>
2024-01-31 14:21:21 +0800

    Update myblood_blast_code.py

--- project_codes/myblood_blast_code.py
+++ project_codes/myblood_blast_code.py
@@ -4,8 +4,9 @@ from datetime import datetime
 def myblood_blast():
     #notify users new report is ready
     tg_token = '<your_tg_bot_token>'
+    chat_id ='<group_chat_id>'
 
     text = (f'Hi, the daily blood donation reports in Malaysia for {datetime.now().strftime("%Y-%m-%d")} is ready.'
             f'\n\nType /start to view.'
             f'\n\nPrepared by @syahiramzar')
-    blast = requests.post(f'https://api.telegram.org/bot{tg_token}/sendMessage?chat_id=<chatid>text={text}')
+    blast = requests.post(f'https://api.telegram.org/bot{tg_token}/sendMessage?chat_id={chat_id}text={text}')
